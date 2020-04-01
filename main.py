3bd8ab313e8b2e7eeb31f53355d4b8e5becbfabd
amirhk <amirhkarimi@gmail.com>
2020-04-01 14:19:31 +0200

    Added code to solve for M2 using grid/exhaustive search over valid action_sets defined by bound and granularity.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import pickle
 import numpy as np
 import pandas as pd
@@ -17,6 +18,7 @@ RANDOM_SEED = 54321
 seed(RANDOM_SEED) # set the random seed so that the random permutations can be reproduced again
 np.random.seed(RANDOM_SEED)
 
+NUMBER_OF_MONTE_CARLO_SAMPLES = 100
 SAVED_MACE_RESULTS_PATH_M0 = '/Users/a6karimi/dev/recourse/_minimum_distances_m0'
 SAVED_MACE_RESULTS_PATH_M1 = '/Users/a6karimi/dev/recourse/_minimum_distances_m1'
 
@@ -38,6 +40,13 @@ def loadDataset():
   return X_train, X_test, y_train, y_test
 
 
+def measureActionSetCost(factual_instance, action_set, norm_type):
+  deltas = []
+  for key in action_set.keys():
+    deltas.append(action_set[key] - factual_instance[key])
+  return np.linalg.norm(deltas, norm_type)
+
+
 def incrementIndices(tmp_dict):
   new_dict = {}
   for key, value in tmp_dict.items():
@@ -50,6 +59,12 @@ def prettyPrintActionSet(action_set):
   for key, value in action_set.items():
     action_set[key] = np.around(value, 2)
   return action_set
+
+
+def prettyPrintInstance(instance):
+  for key, value in instance.items():
+    instance[key] = np.around(value, 2)
+  return instance
 
 
 # TODO: should have a class of defining SCM with this as a method
@@ -93,11 +108,15 @@ def getParents(node):
     return {'x1', 'x2'}
 
 
-def didFlip(factual_instance, counterfactual_instance):
+def getPrediction(instance):
   sklearn_model = loadModel.loadModelForDataset('lr', 'random')
-  factual_prediction = sklearn_model.predict(np.array(list(factual_instance.values())).reshape(1,-1))
-  counterfactual_prediction = sklearn_model.predict(np.array(list(counterfactual_instance.values())).reshape(1,-1))
-  return factual_prediction != counterfactual_prediction
+  prediction = sklearn_model.predict(np.array(list(instance.values())).reshape(1,-1))[0]
+  assert prediction == 0 or prediction == 1, f'Expected prediction in {0,1}; got {prediction}'
+  return prediction
+
+
+def didFlip(factual_instance, counterfactual_instance):
+  return getPrediction(factual_instance) != getPrediction(counterfactual_instance)
 
 
 def getRandomM2Sample(factual_instance, action_set):
@@ -232,21 +251,25 @@ def scatterCounterfactuals(factual_instance, action_set, counterfactual_type, sc
       m2 = getRandomM2Sample(factual_instance, action_set)
       list_m2.append(m2)
       color_string = 'green' if didFlip(factual_instance, m2) else 'red'
-      ax.scatter(m2['x1'], m2['x2'], m2['x3'], marker = '^', color=color_string, alpha=0.1, s=40)
+      ax.scatter(m2['x1'], m2['x2'], m2['x3'], marker = '^', color=color_string, alpha=0.1, s=30)
 
     list_m2_x1 = [elem['x1'] for elem in list_m2]
     list_m2_x2 = [elem['x2'] for elem in list_m2]
     list_m2_x3 = [elem['x3'] for elem in list_m2]
 
-    # TODO: choose 1 shape for each of m0, m1, m2 and choose color based on did_flip
-    # TODO: choose whether to show mean / median or not.. (conflicts with above)
-    # ax.scatter(np.mean(list_m2_x1), np.mean(list_m2_x2), np.mean(list_m2_x3), marker = 'o', color='blue', alpha=0.5, s=70)
+    # TODO: choose whether to show mean / median or not.. (conflicts with above?)
+    mean_m2_instance = {
+      'x1': np.mean(list_m2_x1),
+      'x2': np.mean(list_m2_x2),
+      'x3': np.mean(list_m2_x3),
+    }
+    color_string = 'green' if didFlip(factual_instance, mean_m2_instance) else 'red'
+    ax.scatter(mean_m2_instance['x1'], mean_m2_instance['x2'], mean_m2_instance['x3'], marker = '^', color=color_string, alpha=0.5, s=70)
     # ax.scatter(np.median(list_m2_x1), np.median(list_m2_x2), np.median(list_m2_x3), marker = 's', color='blue', alpha=0.5, s=70)
 
   else:
 
     raise Exception(f'{counterfactual_type} not recognized.')
-
 
 
 def experiment1(X_train, X_test, y_train, y_test):
@@ -299,7 +322,7 @@ def experiment2(X_train, X_test, y_train, y_test):
       ax.set_xlabel('x1')
       ax.set_ylabel('x2')
       ax.set_zlabel('x3')
-      ax.set_title(f'sample_{factual_instance_idx} \n do({prettyPrintActionSet(action_set)})', fontsize=8)
+      ax.set_title(f'sample_{factual_instance_idx} \n do({prettyPrintActionSet(action_set)})', fontsize=8, horizontalalignment='left')
       ax.view_init(elev=15, azim=10)
 
 
@@ -308,15 +331,122 @@ def experiment2(X_train, X_test, y_train, y_test):
       #   pyplot.draw()
       #   pyplot.pause(.001)
 
-  pyplot.suptitle("Compare M0, M1, M2 on <n> factual samples and <n> **fixed** action sets.", fontsize=14)
+  pyplot.suptitle('Compare M0, M1, M2 on <n> factual samples and <n> **fixed** action sets.', fontsize=14)
   pyplot.show()
+
+
+def isM2ConstraintSatisfied(factual_instance, action_set):
+  # monte_carlo_samples = [0] * NUMBER_OF_MONTE_CARLO_SAMPLES
+  monte_carlo_predictions = [0] * NUMBER_OF_MONTE_CARLO_SAMPLES
+  d1 = []
+  d2 = []
+  d3 = []
+  for i in range(NUMBER_OF_MONTE_CARLO_SAMPLES):
+    # s0 = time.time()
+    monte_carlo_sample = getRandomM2Sample(factual_instance, action_set)
+    # d1.append(time.time() - s0)
+    # monte_carlo_samples[i] = monte_carlo_sample
+    # d2.append(time.time() - d1[-1])
+    monte_carlo_predictions[i] = getPrediction(monte_carlo_sample)
+    # d3.append(time.time() - d2[-1])
+
+  # print(f'\nd1 time: {np.mean(d1)}')
+  # print(f'd2 time: {np.mean(d2)}')
+  # print(f'd3 time: {np.mean(d3)}\n')
+
+  # IMPORTANT... WE ARE CONSIDERING {0,1} LABELS AND FACTUAL SAMPLES MAY BE OF
+  # EITHER CLASS. THEREFORE, THE CONSTRAINT IS SATISFIED WHEN SIGNIFICANTLY
+  # > 0.5 OR < 0.5 FOR A FACTUAL SAMPLE WITH Y = 0 OR Y = 1, RESPECTIVELY.
+
+  expectation = np.mean(monte_carlo_predictions)
+  variance = np.sum(np.power(monte_carlo_predictions - expectation, 2)) / (len(monte_carlo_predictions) - 1)
+
+  if getPrediction(factual_instance) == 0:
+    return expectation - 2 * np.sqrt(variance) > 0.5 # NOTE DIFFERNCE IN SIGN OF STD
+  else: # factual_prediction == 1
+    return expectation + 2 * np.sqrt(variance) < 0.5 # NOTE DIFFERNCE IN SIGN OF STD
+
+
+def getOptimalActionSet(factual_instance, factual_instance_idx, counterfactual_type):
+
+  if counterfactual_type == 'm0':
+
+    print(f'\t[INFO] Loading optimal action set for m0 from saved MINT results...', end='')
+    mace_results_m0 = pickle.load(open(SAVED_MACE_RESULTS_PATH_M0, 'rb'))
+    factual_instance_idx_mace = f'sample_{factual_instance_idx}'
+    assert \
+      factual_instance_idx_mace in mace_results_m0.keys(), \
+      f'missing results for `{factual_instance_idx_mace}` in mace_results.'
+    print(f'\t done.')
+    return incrementIndices(mace_results_m0[factual_instance_idx_mace]['action_set'])
+
+  elif counterfactual_type == 'm1':
+
+    print(f'\t[INFO] Loading optimal action set for m1 from saved MINT results...', end='')
+    mace_results_m1 = pickle.load(open(SAVED_MACE_RESULTS_PATH_M1, 'rb'))
+    factual_instance_idx_mace = f'sample_{factual_instance_idx}'
+    assert \
+      factual_instance_idx_mace in mace_results_m1.keys(), \
+      f'missing results for `{factual_instance_idx_mace}` in mace_results.'
+    print(f'\t done.')
+    return incrementIndices(mace_results_m1[factual_instance_idx_mace]['action_set'])
+
+  elif counterfactual_type == 'm2':
+
+    GRANULARITY = 2
+    x1_possible_actions = list(np.around(np.linspace(-2, 2, GRANULARITY+1), 2))
+    x2_possible_actions = list(np.around(np.linspace(-2, 2, GRANULARITY+1), 2))
+    x3_possible_actions = list(np.around(np.linspace(-2, 2, GRANULARITY+1), 2))
+    x1_possible_actions.append('n/a')
+    x2_possible_actions.append('n/a')
+    x3_possible_actions.append('n/a')
+
+    all_action_sets = []
+    for x1_action in x1_possible_actions:
+      for x2_action in x2_possible_actions:
+        for x3_action in x3_possible_actions:
+          all_action_sets.append({
+            'x1': x1_action,
+            'x2': x2_action,
+            'x3': x3_action,
+          })
+
+    # go through, and for any action_set that has a value = 'n/a', remove ONLY
+    # THAT key, value pair, NOT THE ENTIRE action_set.
+    valid_action_sets = []
+    for action_set in all_action_sets:
+      valid_action_sets.append({k:v for k,v in action_set.items() if v != 'n/a'})
+
+    # ipsh()
+    # # FOR TEST PURPOSES ONLY
+    # valid_action_sets = [ \
+    #   {'x1': 1}, \
+    #   {'x2': 1}, \
+    #   {'x3': 1}, \
+    # ]
+    # valid_action_sets = list(np.random.choice(valid_action_sets, 10))
+
+    print(f'\t[INFO] Computing optimal M2: grid searching over {len(valid_action_sets)} action sets...', end='')
+
+    min_cost = 1e3
+    min_cost_action_set = {}
+    for action_set in valid_action_sets:
+      if isM2ConstraintSatisfied(factual_instance, action_set) == True:
+        cost_of_action_set = measureActionSetCost(factual_instance, action_set, 2)
+        if cost_of_action_set < min_cost:
+          min_cost = cost_of_action_set
+          min_cost_action_set = action_set
+
+    print(f'\t done.')
+
+    return min_cost_action_set
+
+  else:
+    raise Exception(f'{counterfactual_type} not recognized.')
 
 
 def experiment3(X_train, X_test, y_train, y_test):
   ''' compare M0, M1, M2 on <n> factual samples and <n> **computed** action sets '''
-
-  mace_results_m0 = pickle.load(open(SAVED_MACE_RESULTS_PATH_M0, 'rb'))
-  mace_results_m1 = pickle.load(open(SAVED_MACE_RESULTS_PATH_M1, 'rb'))
 
   # for
 
@@ -336,8 +466,10 @@ def experiment3(X_train, X_test, y_train, y_test):
 
   # ipsh()
 
-  NUM_SAMPLES = 16
-  NUM_PLOT_ROWS = 4
+  # NUM_SAMPLES = 16
+  # NUM_PLOT_ROWS = 4
+  NUM_SAMPLES = 9
+  NUM_PLOT_ROWS = 3
 
   factual_instances_df = X_test.iloc[:NUM_SAMPLES].copy()
   factual_instances_dict = factual_instances_df.T.to_dict()
@@ -347,10 +479,10 @@ def experiment3(X_train, X_test, y_train, y_test):
   for index, (key, value) in enumerate(factual_instances_dict.items()):
     idx_sample = index
     factual_instance_idx = key
-    factual_instance_idx_mace = f'sample_{factual_instance_idx}'
     factual_instance = value
-    assert factual_instance_idx_mace in mace_results_m0.keys(), f'missing results for `{factual_instance_idx_mace}` in mace_results.'
-    assert factual_instance_idx_mace in mace_results_m0.keys(), f'missing results for `{factual_instance_idx_mace}` in mace_results.'
+
+    print(f'[INFO] Computing counterfactuals (M0, M1, M2) for factual instance #{index+1} / {NUM_SAMPLES} (id #{factual_instance_idx})...')
+
     ax = pyplot.subplot(
       len(factual_instances_dict) // NUM_PLOT_ROWS + int(not(len(factual_instances_dict) % NUM_PLOT_ROWS == 0)),
       NUM_PLOT_ROWS,
@@ -358,20 +490,32 @@ def experiment3(X_train, X_test, y_train, y_test):
       projection = '3d')
     # scatterDataset(X_train, X_test, y_train, y_test, ax)
     scatterFactual(factual_instance, ax)
-    m0_action_set = incrementIndices(mace_results_m0[factual_instance_idx_mace]['action_set'])
-    m1_action_set = incrementIndices(mace_results_m1[factual_instance_idx_mace]['action_set'])
-    scatterCounterfactuals(factual_instance, m0_action_set, 'm0', 'true', ax)
-    scatterCounterfactuals(factual_instance, m1_action_set, 'm1', 'true', ax)
+
+    m0_optimal_action_set = getOptimalActionSet(factual_instance, factual_instance_idx, 'm0')
+    m1_optimal_action_set = getOptimalActionSet(factual_instance, factual_instance_idx, 'm1')
+    m2_optimal_action_set = getOptimalActionSet(factual_instance, factual_instance_idx, 'm2')
+
+    m0_optimal_action_set_cost = measureActionSetCost(factual_instance, m0_optimal_action_set, 2)
+    m1_optimal_action_set_cost = measureActionSetCost(factual_instance, m1_optimal_action_set, 2)
+    m2_optimal_action_set_cost = measureActionSetCost(factual_instance, m2_optimal_action_set, 2)
+
+    scatterCounterfactuals(factual_instance, m0_optimal_action_set, 'm0', 'true', ax)
+    scatterCounterfactuals(factual_instance, m1_optimal_action_set, 'm1', 'true', ax)
+    # scatterCounterfactuals(factual_instance, m1_optimal_action_set, 'm2', 'irrelevant', ax) # how many of m2 suggested by m1 would fail
+    scatterCounterfactuals(factual_instance, m2_optimal_action_set, 'm2', 'irrelevant', ax)
+
     scatterDecisionBoundary(ax)
     ax.set_xlabel('x1')
     ax.set_ylabel('x2')
     ax.set_zlabel('x3')
     ax.set_title(
-      f'sample_{factual_instance_idx}'
-      f'\n m0 action set: do({prettyPrintActionSet(m0_action_set)})'
-      f'\n m1 action set: do({prettyPrintActionSet(m1_action_set)})'
-    , fontsize=8)
+      f'sample_{factual_instance_idx} - {prettyPrintInstance(factual_instance)}'
+      f'\n m0 action set: do({prettyPrintActionSet(m0_optimal_action_set)}); cost: {m0_optimal_action_set_cost:.2f}'
+      f'\n m1 action set: do({prettyPrintActionSet(m1_optimal_action_set)}); cost: {m1_optimal_action_set_cost:.2f}'
+      f'\n m2 action set: do({prettyPrintActionSet(m2_optimal_action_set)}); cost: {m2_optimal_action_set_cost:.2f}'
+    , fontsize=8, horizontalalignment='left')
     ax.view_init(elev=20, azim=-30)
+    print('[INFO] done.')
 
     # for angle in range(0, 360):
     #   ax.view_init(30, angle)
@@ -383,7 +527,7 @@ def experiment3(X_train, X_test, y_train, y_test):
   # fig.legend(handles, labels, loc='upper center')
   # ipsh()
 
-  pyplot.suptitle("Compare M0, M1, M2 on <n> factual samples and <n> **computed** action sets.", fontsize=14)
+  pyplot.suptitle(f'Compare M0, M1, M2 on {NUM_SAMPLES} factual samples and **optimal** action sets.', fontsize=14)
   pyplot.show()
 
 
@@ -403,7 +547,6 @@ def visualizeDatasetAndFixedModel(X_train, X_test, y_train, y_test):
   ax.grid(True)
 
   pyplot.show()
-
 
 
 if __name__ == "__main__":
