12767908c6db207c3f88d04d619d1bf846235761
amirhk <amirhkarimi@gmail.com>
2020-05-31 22:52:06 +0200

    Requesting more MEMORY and CPU on cluster in hopes that jobs don't get put on hold.

--- createSubFile.py
+++ createSubFile.py
@@ -3,10 +3,11 @@ import numpy as np
 SCM_CLASS_VALUES = ['sanity-3-lin', 'sanity-3-anm', 'sanity-3-gen']
 # LAMBDA_LCB_VALUES = np.linspace(0,2.5,6)
 LAMBDA_LCB_VALUES = [1]
-OPTIMIZATION_APPROACHES = ['brute_force', 'grad_descent']
+# OPTIMIZATION_APPROACHES = ['brute_force', 'grad_descent']
+OPTIMIZATION_APPROACHES = ['grad_descent']
 REPEAT_COUNT = 1
 
-request_memory = 8192
+request_memory = 8192*4
 
 sub_file = open('test.sub','w')
 print('executable = /home/amir/dev/recourse/_venv/bin/python', file=sub_file)
@@ -14,7 +15,7 @@ print('error = _cluster_logs/test.$(Process).err', file=sub_file)
 print('output = _cluster_logs/test.$(Process).out', file=sub_file)
 print('log = _cluster_logs/test.$(Process).log', file=sub_file)
 print(f'request_memory = {request_memory}', file=sub_file)
-print('request_cpus = 2', file=sub_file)
+print('request_cpus = 4', file=sub_file)
 print('\n' * 2, file=sub_file)
 
 for scm_class in SCM_CLASS_VALUES:
