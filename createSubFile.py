1e3325dca3f4fb0defcce46269a5ae5f33e21c0d
amirhk <amirhkarimi@gmail.com>
2020-06-09 20:26:47 +0200

    Post-bug-fixes (not enough samples + perma-cache issues)
    Re-run (test repro of main body table 1 + create equiv table using BF for suppl)
    
    sanity-3-{lin,anm,gen} x gamma_lcb = {2} x {BF_20, GD_1000} x 8 recourse types x lr

--- createSubFile.py
+++ createSubFile.py
@@ -2,7 +2,7 @@ import numpy as np
 
 SCM_CLASS_VALUES = ['sanity-3-lin', 'sanity-3-anm', 'sanity-3-gen']
 LAMBDA_LCB_VALUES = [2.]
-OPTIMIZATION_APPROACHES = ['grad_descent']
+OPTIMIZATION_APPROACHES = ['brute_force', 'grad_descent']
 CLASSIFIER_VALUES = ['lr']
 
 # ==============================================================================
