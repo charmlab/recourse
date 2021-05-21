813d5e0a91ca067b19ca372cc7f432fa1d83d8c9
Amir-Hossein Karimi <amirhk@student-net-vpn-1622.ethz.ch>
2021-05-21 21:08:43 +0430

    Minor updates to prepareLaTeXTables.py

--- prepareLaTeXTables.py
+++ prepareLaTeXTables.py
@@ -15,15 +15,15 @@ for approx_scm in ['m0_true', 'm1_alin', 'm1_akrr']:
     elif approx_scm == 'm1_akrr':
         print(r'\multirow{5}{*}{$\Hat{\Mcal}_{\text{KR}}$}')
     for fair_model in [
-        'vanilla_svm',
-        'nonsens_svm',
-        'iw_fair_svm',
-        'unaware_svm',
-        'cw_fair_svm',
-        # 'vanilla_*', # lr/mlp, depending on scm_setup
-        # 'nonsens_*', # lr/mlp, depending on scm_setup
-        # 'unaware_*', # lr/mlp, depending on scm_setup
-        # 'cw_fair_*', # lr/mlp, depending on scm_setup
+        # 'vanilla_svm',
+        # 'nonsens_svm',
+        # 'iw_fair_svm',
+        # 'unaware_svm',
+        # 'cw_fair_svm',
+        'vanilla_*', # lr/mlp, depending on scm_setup
+        'nonsens_*', # lr/mlp, depending on scm_setup
+        'unaware_*', # lr/mlp, depending on scm_setup
+        'cw_fair_*', # lr/mlp, depending on scm_setup
         ]:
         if fair_model == 'vanilla_svm':
             print('\t' + r'& SVM$(\Xb, A)$')
@@ -51,15 +51,15 @@ for approx_scm in ['m0_true', 'm1_alin', 'm1_akrr']:
             'fair-CAU-LIN-radial__',
             'fair-CAU-ANM-radial__',
         ]:
-            if '*' in fair_model:
+            if 'svm' not in fair_model:
                 if 'radial' not in scm_setup:
-                    fair_model = fair_model[:-1] + 'lr'
+                    fair_model = fair_model[:8] + 'lr'
                 else:
-                    fair_model = fair_model[:-1] + 'mlp'
+                    fair_model = fair_model[:8] + 'mlp'
 
             # find folder
-            folders = glob.glob(f'./_experiments/_fair_neurips/_bu_table1,2/*{scm_setup}*{fair_model}*')
-            # folders = glob.glob(f'./_experiments/_fair_neurips/_table1,2/*{scm_setup}*{fair_model}*')
+            # folders = glob.glob(f'./_experiments/_fair_neurips_old/_bu_table1,2/*{scm_setup}*{fair_model}*')
+            folders = glob.glob(f'./_experiments/_fair_neurips_old/_table1,2/*{scm_setup}*{fair_model}*')
             assert len(folders) == 1
             folder_path = folders[0]
 
