4cba6156ca87184734fef3c99f1eb7cc21ca2a54
Amir-Hossein Karimi <amirhk@student-net-vpn-1369.ethz.ch>
2021-05-22 19:06:17 +0430

    [MAJOR] s/factual_instance/factual_instance_obj in most places of codebase (except for factual_instance_ts; TODO)

--- sandbox.sh
+++ sandbox.sh
@@ -5,3 +5,141 @@ zip -r _fair_neurips 2021.05.20_11.13*
 
 # on local machine
 scp -r amir@login.cluster.is.localnet:~/dev/recourse/_experiments/_fair_neurips.zip _fair_neurips
+
+
+python main.py \
+	--scm_class sanity-3-lin \
+	--classifier_class lr \
+	--lambda_lcb 1 \
+	--optimization_approach brute_force \
+	--grid_search_bins 4 -e 6 \
+	--num_train_samples 500 \
+	--num_fair_samples 2 \
+	--batch_number 0 \
+	--sample_count 3
+
+python main.py \
+	--scm_class sanity-3-anm \
+	--classifier_class lr \
+	--lambda_lcb 1 \
+	--optimization_approach grad_descent \
+	--grid_search_bins 4 -e 6 \
+	--num_train_samples 500 \
+	--num_fair_samples 2 \
+	--batch_number 0 \
+	--sample_count 3
+
+python main.py \
+	--scm_class sanity-3-gen \
+	--classifier_class mlp \
+	--lambda_lcb 1 \
+	--optimization_approach grad_descent \
+	--grid_search_bins 4 -e 6 \
+	--num_train_samples 500 \
+	--num_fair_samples 2 \
+	--batch_number 0 \
+	--sample_count 3
+
+python main.py \
+	--scm_class german-credit \
+	--classifier_class mlp \
+	--lambda_lcb 1 \
+	--optimization_approach brute_force \
+	--grid_search_bins 2 -e 6 \
+	--num_train_samples 500 \
+	--num_fair_samples 2 \
+	--batch_number 0 \
+	--sample_count 3
+
+python main.py \
+	--scm_class german-credit \
+	--classifier_class tree \
+	--lambda_lcb 1 \
+	--optimization_approach brute_force \
+	--grid_search_bins 2 -e 6 \
+	--num_train_samples 500 \
+	--num_fair_samples 2 \
+	--batch_number 0 \
+	--sample_count 3
+
+
+
+python main.py \
+	--scm_class adult \
+	--dataset_class adult \
+	--classifier_class vanilla_mlp \
+	--lambda_lcb 1 \
+	--optimization_approach brute_force \
+	--grid_search_bins 3 -e 9 \
+	--sensitive_attribute_nodes x1 \
+	--non_intervenable_nodes x2 x3 \
+	--num_train_samples 500 \
+	--num_fair_samples 2 \
+	--batch_number 0 \
+	--sample_count 100
+
+
+
+
+python main.py \
+	--scm_class fair-IMF-LIN \
+	--classifier_class vanilla_mlp \
+	--lambda_lcb 1 \
+	--optimization_approach brute_force \
+	--grid_search_bins 3 -e 9 \
+	--sensitive_attribute_nodes x1 \
+	--num_train_samples 500 \
+	--num_fair_samples 2 \
+	--batch_number 0 \
+	--sample_count 200
+
+python main.py \
+	--scm_class fair-IMF-LIN \
+	--classifier_class nonsens_lr \
+	--lambda_lcb 1 \
+	--optimization_approach brute_force \
+	--grid_search_bins 3 -e 9 \
+	--sensitive_attribute_nodes x1 \
+	--num_train_samples 500 \
+	--num_fair_samples 2 \
+	--batch_number 0 \
+	--sample_count 200
+
+python main.py \
+	--scm_class fair-IMF-LIN-radial \
+	--classifier_class iw_fair_svm \
+	--lambda_lcb 1 \
+	--optimization_approach brute_force \
+	--grid_search_bins 3 -e 9 \
+	--sensitive_attribute_nodes x1 \
+	--num_train_samples 500 \
+	--num_fair_samples 2 \
+	--batch_number 0 \
+	--sample_count 200
+
+python main.py \
+	--scm_class fair-IMF-LIN-radial \
+	--classifier_class unaware_svm \
+	--lambda_lcb 1 \
+	--optimization_approach brute_force \
+	--grid_search_bins 3 -e 9 \
+	--sensitive_attribute_nodes x1 \
+	--num_train_samples 500 \
+	--num_fair_samples 2 \
+	--batch_number 0 \
+	--sample_count 200
+
+python main.py \
+	--scm_class fair-IMF-LIN-radial \
+	--classifier_class cw_fair_svm \
+	--lambda_lcb 1 \
+	--optimization_approach brute_force \
+	--grid_search_bins 3 -e 9 \
+	--sensitive_attribute_nodes x1 \
+	--num_train_samples 500 \
+	--num_fair_samples 2 \
+	--batch_number 0 \
+	--sample_count 200
+
+
+
